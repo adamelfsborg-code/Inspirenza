e1feaaf00a1388e17f172372feeb983778e849f7
Adam Elfsborg <adam.elfsborg@mtek.se>
2023-11-12 14:51:00 +0100

    feat: find first word

--- programming-problems/find-first-word/src/main.rs
+++ programming-problems/find-first-word/src/main.rs
@@ -1,3 +1,23 @@
+use std::io::{stdin,stdout,Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut sentence=String::new();
+    loop {
+        print!("Please enter some text: ");
+        stdout().flush().unwrap();
+        match stdin().read_line(&mut sentence) {
+            Err(err) => {
+                println!("{err}")
+            },
+            Ok(_) => {break}
+        }
+    }
+    let word = find_first_word(&mut sentence);
+    println!("The first word is {}", word)
+}
+
+
+fn find_first_word(sentence: &mut String) -> &str {
+    let mut word = sentence.split(' ');
+    word.next().unwrap()
 }
